53ae0ac9350792d98e8d51b9284114f544922e18
swaroop1232010 <76110428+swaroop1232010@users.noreply.github.com>
2024-09-04 03:04:32 -0400

    updated redis blog

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -21,13 +21,15 @@ const Blog = () => (
           <div className="w-fit px-[2.5rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
             Apr 7, 2023
           </div>
-          <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
-            {/* <div className="flex items-center space-x-3">
+          {/* <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4"> */}
+          {/* <div className="flex items-center space-x-3">
                 <UserCircleIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]" />
                 <p className="text-white">By Swaroop Reddy</p>
               </div> */}
-          </div>
-          <p className="mt-[1rem] text-white text-[18px] font-semibold">3 New API’s In React 18</p>
+          {/* </div> */}
+          <p className="mt-[1rem] text-white text-[18px] font-semibold">
+            3 New API&apos;s Every Developer Should Know In React 18
+          </p>
         </div>
       </div>
       <div data-aos="fade-up" data-aos-delay="300">
@@ -42,41 +44,40 @@ const Blog = () => (
           <div className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
             Aug 31, 2024
           </div>
-          <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
-            {/* <div className="flex items-center space-x-3">
+          {/* <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4"> */}
+          {/* <div className="flex items-center space-x-3">
                 <UserCircleIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]" />
                 <p className="text-white">By Swaroop Reddy</p>
               </div> */}
-          </div>
+          {/* </div> */}
           <p className="mt-[1rem] text-white text-[18px] font-semibold">
-            Java X SonarQube.
+            Implementation of SonarQube in Spring Boot.
           </p>
         </div>
       </div>
-      {/* <div data-aos="fade-up" data-aos-delay="600">
-          <div className="w-[100%] relative h-[400px]">
-            <Image
-              src="./images/b1.jpg"
-              alt="blog"
-              layout="fill"
-              className="object-cover"
-            />
+      <div data-aos="fade-up" data-aos-delay="600">
+        <Link
+          href="https://medium.com/@swaroop1232010/implementing-redis-caching-in-spring-boot-with-gradle-a-step-by-step-guide-876884df0bb6"
+          target="_blank"
+          className="block inset-0 w-[100%] relative h-[400px]"
+        >
+          <Image src="./images/b3.jpg" alt="blog" layout="fill" className="object-cover" />
+        </Link>
+        <div className="w-[90%] text-center mx-auto bg-[#09101a] relative p-[2rem] mt-[-1rem]">
+          <div className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
+            Sep 4, 2024
           </div>
-          <div className="w-[90%] text-center mx-auto bg-[#09101a] relative p-[2rem] mt-[-1rem]">
-            <div className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
-              Apr 7, 2023
-            </div>
-            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
-              <div className="flex items-center space-x-3">
+          {/* <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4"> */}
+          {/* <div className="flex items-center space-x-3">
                 <UserCircleIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]" />
                 <p className="text-white">By Swaroop Reddy</p>
-              </div>
-            </div>
-            <p className="mt-[1rem] text-white text-[18px] font-semibold">
-              NextJS Fullstack Development
-            </p>
-          </div>
-        </div> */}
+              </div> */}
+          {/* </div> */}
+          <p className="mt-[1rem] text-white text-[18px] font-semibold">
+            Implementation of Redis Caching in Spring Boot with Gradle.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 );
